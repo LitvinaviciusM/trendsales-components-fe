bb8a2a4fd46b393008e6ded0a778d28a51be30ca
Martynas Litvinavicius <litvinaviciusm@gmail.com>
2022-05-30 16:51:53 +0300

    Initial storybook setup

---
+++ src/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './button.css';
+
+export interface Props extends React.HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant: 'primary' | 'secondary';
+};
+
+export const Button = ({ children, className = '', variant, ...props }: Props) => {
+  return (
+    <button
+      className={`tk-soleil button ${variant} ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
---
+++ stories/Button.stories.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { Button, Props } from '../src/Button';
+import { action } from '@storybook/addon-actions';
+
+const meta: Meta = {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    onClick: action('clicked'),
+  }
+}
+export default meta;
+
+const Template: Story<Props> = (args) => <Button {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Primary',
+  variant: 'primary',
+  disabled: true,
+}
