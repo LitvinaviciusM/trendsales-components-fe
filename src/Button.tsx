cb494d48fbe1e12bfb634d5dbeedeab495f0915e
Martynas Litvinavicius <litvinaviciusm@gmail.com>
2022-05-31 14:11:52 +0300

    Upgraded to webpack 5. Using preview-head.html to load fonts and global styles. Configured prettier.

--- src/Button.tsx
+++ src/Button.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import './button.css';
 
 export interface Props extends React.HTMLAttributes<HTMLButtonElement> {
-  children: React.ReactNode;
-  variant: 'primary' | 'secondary';
-};
+	children: React.ReactNode;
+	variant: 'primary' | 'secondary';
+}
 
-export const Button = ({ children, className = '', variant, ...props }: Props) => {
-  return (
-    <button
-      className={`tk-soleil button ${variant} ${className}`}
-      {...props}
-    >
-      {children}
-    </button>
-  );
+export const Button = ({
+	children,
+	className = '',
+	variant,
+	...props
+}: Props) => {
+	return (
+		<button className={`tk-soleil button ${variant} ${className}`} {...props}>
+			{children}
+		</button>
+	);
 };
