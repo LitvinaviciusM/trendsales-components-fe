f7d7d54115b20495773b88687bcc1736ea68d760
Martynas Litvinavicius <litvinaviciusm@gmail.com>
2022-05-31 19:42:05 +0300

    Initial babel setup. Support for asssets import when running Jest.

--- src/Button.test.js
+++ src/Button.test.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import { Button } from './Button';
 
-test('has an onclick attribute when rendering', () => {
+test('children matches text content', () => {
 	const { container } = render(<Button variant="primary">Primary</Button>);
 	const buttonElement = container.querySelector('button');
 	expect(buttonElement).not.toBeNull();
